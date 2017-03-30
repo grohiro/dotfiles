d958983774b0b0bf89e71986857efc8788abed86
grohiro <khirokawa@growk.jp>
2017-03-30 23:52:20 +0900

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DOT_FILES=( .bashrc .bashrc.aliases .bashrc.color .bashrc.env .bashrc.functions .gitconfig .profile .screenrc .vimrc)
+DOT_DIRS=( .lftp)
+
+# dot files
+for file in ${DOT_FILES[@]}
+do
+  ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+# dot directories
+for dir in ${DOT_DIRS[@]}
+do
+  mkdir -p $HOME/$dir
+  for file in $(find $HOME/dotfiles/$dir -type f -depth 1)
+  do
+    fname=$(basename $file)
+    ln -s $file $HOME/$dir/$fname
+  done
+done
